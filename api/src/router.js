30b82cfbe6b93a319c13627b37299a5ce8ae4be6
James Alt <james.alt@me.com>
2020-10-12 11:34:02 -0500

    Module 3 work completed

--- api/src/router.js
+++ api/src/router.js
@@ -1,10 +1,11 @@
-const { Router } = require('express');
+const {Router}= require('express');
 const multer = require('multer');
+const path = require('path');
 
-const router = Router();
+const router = new Router();
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
-
-let filename = (request, file, callback) => {
+const filename = (request, file, callback) => {
   callback(null, file.originalname);
 };
 
@@ -13,22 +14,26 @@ const storage = multer.diskStorage({
   filename,
 });
 
-let fileFilter = (request, file, callback) => {
+const fileFilter = (request, file, callback) => {
   if (file.mimetype !== 'image/png') {
     request.fileValidationError = 'Wrong file type';
     callback(null, false, new Error('Wrong file type'));
   } else {
     callback(null, true);
   }
-}
+};
 
-const upload = multer({ fileFilter, storage });
+const upload = multer({fileFilter, storage});
 
 router.post('/upload', upload.single('photo'), (request, response) => {
-  if(request.fileValidationError) {
-    return response.status(400).json({ error: request.fileValidationError });
+  if (request.fileValidationError) {
+    return response.status(400).json({error: request.fileValidationError});
   }
-  return response.status(201).json({ success: true });
+  return response.status(201).json({success: true});
+});
+
+router.get('/photo-viewer', (request, response) => {
+  response.sendFile(photoPath);
 });
 
 module.exports = router;
